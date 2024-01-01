d9da01b67390626e2acfe9132a944cc7bc4ba971
Shubhayan Srivastava <88150274+shubhayan935@users.noreply.github.com>
2024-01-01 16:11:50 +0530

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+import cv2
+from deepface import DeepFace
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/detect_emotion', methods=['POST'])
+def detect_emotion():
+  frame = cv2.imread("photo.jpg")
+  face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+  face = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5)
+
+  for x, y, w, h in face:
+    image = cv2.rectangle(frame, (x, y), (x + w, y + h), (89, 2, 236), 1)
+    analyze = DeepFace.analyze(frame, actions=['emotion'])
+    cv2.putText(image, analyze[0]['dominant_emotion'], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (224, 77, 176), 2)
+
+  (frame)
+  return jsonify(emotion='happy')
+
+if __name__ == '__main__':
+    app.run()
